a67aaaa5ba77615b86f690b3ce64872dc5a5da68
Rajesh <talaganarajesh@gmail.com>
2025-04-23 15:05:24 +0530

    seo keywords and title updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,9 +17,42 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Talagana Rajesh",
-  description: "Portfolio website of Talagana Rajesh",
-  icons: "/myphoto.jpg"
+  title: "Talagana Rajesh-Full stack web developer",
+  authors: [{ name: "Talagana Rajesh" }],
+  description: "Portfolio website of Talagana Rajesh-Founder of Get Material",
+  icons: "/myphoto.jpg",
+  keywords: [
+    "Talagana Rajesh",
+    "Portfolio",
+    "Full stack web developer",
+    "Full stack developer in NIST",
+    "Get Material",
+    "Web development",
+    "Software engineer",
+    "Software engineer in NIST",
+    "Software engineer in Berhampur",
+    "Web Developer in Berhampur",
+    "Website Developer in Berhampur",
+    "Web Developer in India",
+    "Website Developer in India",
+    "Web Developer in Odisha",
+    "Website Developer in Odisha",
+    "Web Developer in Berhampur Odisha",
+    "Website Developer in Berhampur Odisha",
+    "Founder of Get Material",
+    "Founder of Get Material nist notes",
+    "Founder of Get Material notes",
+    "Founder of Get Material notes nist",
+    "Rajesh nist",
+    "Rajesh Get Material",
+    "Rajesh Get Material nist notes",
+    "Rajesh Get Material nist",
+    "Rajesh Get Material notes",
+    "Rajesh Get Material notes nist",
+    "Talagana Rajesh nist",
+    "Talagana Rajesh Get Material",
+    "Talagana Rajesh berhampur",
+  ],
 };
 
 
