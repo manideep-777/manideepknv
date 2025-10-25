bf322bb8aa8fc7f78082e21ac16eede6580f8066
mani <manideepknv@gmail.com>
2025-10-25 21:05:44 +0530

    resolved deployment

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://manideepknv-7asm.vercel.app',
-    generateRobotsTxt: true,
-  }
-  
+  siteUrl: 'https://manideepknv-7asm.vercel.app', // ✅ your actual site URL
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 7000,
+}
