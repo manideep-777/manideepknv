dc6357935c93034bbf5eb5154f32504b81b23917
mani <manideepknv@gmail.com>
2025-10-25 21:24:20 +0530

    Remove SEO files (robots.txt, sitemap)

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://manideepknv-7asm.vercel.app', // ✅ your actual site URL
-  generateRobotsTxt: true,
-  changefreq: 'weekly',
-  priority: 0.7,
-  sitemapSize: 7000,
-}
